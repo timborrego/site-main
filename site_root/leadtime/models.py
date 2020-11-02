9e16a3f4fbfb6f3bc39e1c0c53a94f8ee10db424
Tim Borrego <61977496+timformlabs@users.noreply.github.com>
2020-11-02 08:35:41 -0500

    Added leadtime app to settings, created Inventory model

--- site_root/leadtime/models.py
+++ site_root/leadtime/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Inventory(models.Model):
+    item = models.TextField()
+    sku = models.TextField()
+    shipper = models.TextField()
+    ship_loc = models.TextField()
+    ship_intransit = models.TextField()
+    ship_backorder = models.TextField()
+    ship_perweek = models.TextField()
+    leadtime_core = models.TextField()
+    leadtime_growth = models.TextField()
